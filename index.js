d7e89796cbade6bee53cab3b270acdb67b2227bf
moiz-xyz <a.moizniaxi123@gmail.com>
2026-03-31 17:57:31 +0500

    update port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import connectDB from "./db/index.js";
 import docRoutes from "./routes/docRoutes.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
